5b00eb06abc249802e66fbf6b70c01c838b01571
Jordan <jcr.salazar@outlook.com>
2020-01-22 14:43:59 -0500

    MS created.

---
+++ src/main/java/com/rafalazar/bootcamp/app/ServicePlazoFijoVipApplication.java
@@ -0,0 +1,13 @@
+package com.rafalazar.bootcamp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicePlazoFijoVipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicePlazoFijoVipApplication.class, args);
+	}
+
+}
